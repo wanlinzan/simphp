e702553e0cd52a51b93f7216374a452fb0c71c55
799034851 <799034851@qq.com>
2017-03-24 17:44:17 +0800

    图片操作类升级

--- index.php
+++ index.php
@@ -4,15 +4,7 @@ include './simphp/autoload.php';
 $app = new \simphp\App();
 
 
-$app->get('/', function ($args) {
-
-
-//    $image = \org\Image::createFromFilename('https://www.baidu.com/img/bd_logo1.png');
-    $image = \org\Image::createFromFilename('38.jpg');
-
-//    p($image);
-
-    $image->thumb(400,400)->save(ROOT_PATH.'AAA.PNG');
+$app->get('/', function () {
 
 });
 
--- simphp/Image.php
+++ simphp/Image.php
@@ -1,258 +1,242 @@
 <?php
 namespace simphp;
-/*
-图像处理类
- */
-class Image {
-    /* 图片保存的路径 */
-    private $path;
 
-    /**
-     * 实例图像对象时传递图像的一个路径，默认值是当前目录
-     * @param  string $path  可以指定处理图片的路径
-     */
-    function __construct($path="./"){
-        $this->path = rtrim($path,"/")."/";
+class Image
+{
+    private $_resource = null;
+
+    private $_width = 0;
+
+    private $_height = 0;
+
+    public function __construct($resource = null)
+    {
+        if (!is_resource($resource)) {
+            throw new \Exception('不是一个有效的图片资源');
+        }
+        $this->_resource = $resource;
+        $this->_width = imagesx($resource);
+        $this->_height = imagesy($resource);
+    }
+
+    public function width()
+    {
+        return $this->_width;
+    }
+
+    public function height()
+    {
+        return $this->_height;
+    }
+
+    public function resource()
+    {
+        return $this->_resource;
     }
 
     /**
-     * 对指定的图像进行缩放
-     * @param  string $name  是需要处理的图片名称
-     * @param  int $width   缩放后的宽度
-     * @param  int $height   缩放后的高度
-     * @param  string $qz   是新图片的前缀
-     * @return mixed      是缩放后的图片名称,失败返回false;
+     * 通过字符串创建图像资源
+     * @param $string
+     * @return Image
+     * @throws \Exception
      */
-    function thumb($name, $width, $height,$qz="th_"){
-        /* 获取图片宽度、高度、及类型信息 */
-        $imgInfo = $this->getInfo($name);
-        /* 获取背景图片的资源 */
-        $srcImg = $this->getImg($name, $imgInfo);
-        /* 获取新图片尺寸 */
-        $size = $this->getNewSize($name,$width, $height,$imgInfo);
-        /* 获取新的图片资源 */
-        $newImg = $this->kidOfImage($srcImg, $size,$imgInfo);
-        /* 通过本类的私有方法，保存缩略图并返回新缩略图的名称，以"th_"为前缀 */
-        return $this->createNewImage($newImg, $qz.$name,$imgInfo);
+    public static function createFromString($string = null)
+    {
+        if (is_null($string) || !is_string($string)) {
+            throw new \Exception('请传入合法的字符串作为参数');
+        }
+        $resource = imagecreatefromstring($string);
+        if (false === $resource) {
+            throw new \Exception('创建图片失败');
+        }
+        return new self($resource);
     }
 
-    /*
-     * @param  int $waterPos    水印位置，有10种状态，0为随机位置；
-     *                1为顶端居左，2为顶端居中，3为顶端居右；
-     *                4为中部居左，5为中部居中，6为中部居右；
-     *                7为底端居左，8为底端居中，9为底端居右；
+    /**
+     * 通过文件名创建图片,支持远程图片
+     * @param null $filename
+     * @return Image
+     * @throws \Exception
      */
-    function waterMark($groundName, $waterName, $waterPos=0, $qz="wa_"){
-        /*获取水印图片是当前路径，还是指定了路径*/
-        $curpath = rtrim($this->path,"/")."/";
-        $dir = dirname($waterName);
-        if($dir == "."){
-            $wpath = $curpath;
-        }else{
-            $wpath = $dir."/";
-            $waterName = basename($waterName);
+    public static function createFromFilename($filename = null)
+    {
+        if (is_null($filename)) {
+            throw new \Exception('请传入合法的路径作为参数');
+        }
+        if (stripos($filename, 'http') !== 0 && !is_file($filename)) {
+            throw new \Exception('文件不存在');
+        }
+        $type = exif_imagetype($filename);
+        if (false === $type) {
+            throw new \Exception('非法图片');
+        }
+        $func = 'imagecreatefrom' . image_type_to_extension($type, false);
+        $resource = $func($filename);
+        if (false === $resource) {
+            throw new \Exception('创建图片失败');
+        }
+        return new self($resource);
+    }
+
+    /**
+     * 压缩图片
+     * @param null $width int 宽度
+     * @param null $height int 高度
+     * @return $this|Image
+     * @throws \Exception
+     */
+    public function thumb($width = null, $height = null)
+    {
+        if (is_null($width) && is_null($height)) {
+            return $this;
         }
 
-        /*水印图片和背景图片必须都要存在*/
-        if(file_exists($curpath.$groundName) && file_exists($wpath.$waterName)){
-            $groundInfo = $this->getInfo($groundName);        //获取背景信息
-            $waterInfo = $this->getInfo($waterName, $dir);      //获取水印图片信息
-            /*如果背景比水印图片还小，就会被水印全部盖住*/
-            if(!$pos = $this->position($groundInfo, $waterInfo, $waterPos)){
-                throw new \Exception('水印不应该比背景图片小!');
+        if (is_null($width)) {
+            if ($height < 1) {
+                throw new \Exception('高度不能小于1');
             }
+            $proportion = $height / $this->_height;
+            $width = max(1, intval($this->_width * $proportion));
+        }
 
-            $groundImg = $this->getImg($groundName, $groundInfo);  //获取背景图像资源
-            $waterImg = $this->getImg($waterName, $waterInfo, $dir); //获取水印图片资源
+        if (is_null($height)) {
+            if ($width < 1) {
+                throw new \Exception('宽度不能小于1');
+            }
+            $proportion = $width / $this->_width;
+            $height = max(1, intval($this->_height * $proportion));
+        }
 
-            /* 调用私有方法将水印图像按指定位置复制到背景图片中 */
-            $groundImg = $this->copyImage($groundImg, $waterImg, $pos, $waterInfo);
-            /* 通过本类的私有方法，保存加水图片并返回新图片的名称，默认以"wa_"为前缀 */
-            return $this->createNewImage($groundImg, $qz.$groundName, $groundInfo);
+        $resource = imagecreatetruecolor($width, $height);
+        imagecopyresized($resource, $this->_resource, 0, 0, 0, 0, $width, $height, $this->_width, $this->_height);
+        return new self($resource);
+    }
 
-        }else{
-            throw new \Exception('图片或水印图片不存在!');
+    /**
+     * 裁剪
+     * @param $x int x坐标
+     * @param $y int y坐标
+     * @param $width int 宽度
+     * @param $height int 高度
+     * @return Image
+     * @throws \Exception
+     */
+    public function crop($x, $y, $width, $height)
+    {
+        if ($x < 0 || $y < 0 || $x + $width > $this->_width || $y + $height > $this->_height) {
+            throw new \Exception('裁剪超过原图大小');
+        }
+        $resource = imagecreatetruecolor($width, $height);
+        imagecopyresampled($resource, $this->_resource, 0, 0, $x, $y, $width, $height, $width, $height);
+        return new self($resource);
+    }
+
+    /**
+     * 合并图片  用于水印图
+     * @param Image $image
+     * @param int $x x坐标
+     * @param int $y y坐标
+     * @param int $alpha 透明度
+     * @return $this
+     * @throws \Exception
+     */
+    public function merge(Image $image, $x = 0, $y = 0, $alpha = 50)
+    {
+        if (!($image instanceof self)) {
+            throw new \Exception('不是一个合法的对象');
+        }
+
+        if ($x < 0 || $y < 0 || $x > $this->_width || $y > $this->_height) {
+            return $this;
+        }
+        //防止透明颜色丢失
+        $src = imagecreatetruecolor($this->_width, $this->_height);
+        $color = imagecolorallocate($src, 255, 255, 255);
+        imagecolortransparent($src, $color);
+        imagefill($src, 0, 0, $color);
+        imagecopy($src, $this->_resource, 0, 0, 0, 0, $this->_width, $this->_height);
+        imagecopy($src, $image->resource(), $x, $y, 0, 0, $image->width(), $image->height());
+        imagecopymerge($this->_resource, $src, 0, 0, 0, 0, $this->_width, $this->_height, $alpha);
+        imagedestroy($src);
+        return $this;
+    }
+
+
+    /**
+     * 写文本
+     * @param $text string 文本
+     * @param $font string 字体文件
+     * @param int $x 坐标
+     * @param int $y 坐标
+     * @param int $size 字体大小
+     * @param string|array $color 颜色
+     * @param int $angle 旋转角度
+     * @return $this
+     * @throws \Exception
+     */
+    public function text($text, $font, $x = 0, $y = 0, $size = 16, $color = '#00000000', $angle = 0)
+    {
+        if (!is_file($font)) {
+            throw new \Exception('字符文件不存在' . $font);
+        }
+        //计算初始位置
+        $info = imagettfbbox($size, $angle, $font, $text);
+        $x += abs(min($info[0], $info[2], $info[4], $info[6])) + 2;
+        $y += abs(min($info[1], $info[3], $info[5], $info[7]));
+
+        //计算颜色
+        if (is_string($color) && 0 === strpos($color, '#')) {
+            $color = str_split(substr($color, 1), 2);
+            $color = array_map('hexdec', $color);
+            if (empty($color[3]) || $color[3] > 127) {
+                $color[3] = 0;
+            }
+        } else if (!is_array($color)) {
+            throw new \Exception('错误的颜色值');
+        }
+        $col = imagecolorallocatealpha($this->_resource, $color[0], $color[1], $color[2], $color[3]);
+        imagettftext($this->_resource, $size, $angle, $x, $y, $col, $font, $text);
+        return $this;
+    }
+
+
+    /**
+     * 显示图片
+     * @param string $type
+     * @param int $quality
+     */
+    public function show($type = 'png', $quality = 80)
+    {
+        header('Content-type:image/' . $type);
+        $func = 'image' . $type;
+        if ($type == 'png') {
+            imagesavealpha($this->_resource, true);
+            imagepng($this->_resource, null, $quality / 10);
+        } else {
+
+            $func($this->_resource, null, $quality);
         }
     }
 
     /**
-     * 在一个大的背景图片中剪裁出指定区域的图片
-     * @param  string $name  需要剪切的背景图片
-     * @param  int $x     剪切图片左边开始的位置
-     * @param  int $y     剪切图片顶部开始的位置
-     * @param  int $width   图片剪裁的宽度
-     * @param  int $height   图片剪裁的高度
-     * @param  string $qz   新图片的名称前缀
-     * @return  mixed      裁剪后的图片名称,失败返回false;
+     * 保存图片
+     * @param null $filename
+     * @param $type
+     * @param int $quality
      */
-    function cut($name, $x, $y, $width, $height, $qz="cu_"){
-        $imgInfo=$this->getInfo($name);         //获取图片信息
-        /* 裁剪的位置不能超出背景图片范围 */
-        if( (($x+$width) > $imgInfo['width']) || (($y+$height) > $imgInfo['height'])){
-            throw new \Exception('裁剪的位置超出了背景图片范围!');
+    public function save($filename = null, $type = 'png', $quality = 80)
+    {
+        if ($type == 'png') {
+            imagesavealpha($this->_resource, true);
+            imagepng($this->_resource, $filename, $quality / 10);
+        } else {
+            $func = 'image' . $type;
+            $func($this->_resource, $filename, $quality);
         }
-
-        $back = $this->getImg($name, $imgInfo);     //获取图片资源
-        /* 创建一个可以保存裁剪后图片的资源 */
-        $cutimg = imagecreatetruecolor($width, $height);
-        /* 使用imagecopyresampled()函数对图片进行裁剪 */
-        imagecopyresampled($cutimg, $back, 0, 0, $x, $y, $width, $height, $width, $height);
-        imagedestroy($back);
-        /* 通过本类的私有方法，保存剪切图并返回新图片的名称，默认以"cu_"为前缀 */
-        return $this->createNewImage($cutimg, $qz.$name,$imgInfo);
     }
 
-    /* 内部使用的私有方法，用来确定水印图片的位置 */
-    private function position($groundInfo, $waterInfo, $waterPos){
-        /* 需要加水印的图片的长度或宽度比水印还小，无法生成水印 */
-        if( ($groundInfo["width"]<$waterInfo["width"]) || ($groundInfo["height"]<$waterInfo["height"]) ) {
-            return false;
-        }
-        switch($waterPos) {
-            case 1:     //1为顶端居左
-                $posX = 0;
-                $posY = 0;
-                break;
-            case 2:     //2为顶端居中
-                $posX = ($groundInfo["width"] - $waterInfo["width"]) / 2;
-                $posY = 0;
-                break;
-            case 3:     //3为顶端居右
-                $posX = $groundInfo["width"] - $waterInfo["width"];
-                $posY = 0;
-                break;
-            case 4:     //4为中部居左
-                $posX = 0;
-                $posY = ($groundInfo["height"] - $waterInfo["height"]) / 2;
-                break;
-            case 5:     //5为中部居中
-                $posX = ($groundInfo["width"] - $waterInfo["width"]) / 2;
-                $posY = ($groundInfo["height"] - $waterInfo["height"]) / 2;
-                break;
-            case 6:     //6为中部居右
-                $posX = $groundInfo["width"] - $waterInfo["width"];
-                $posY = ($groundInfo["height"] - $waterInfo["height"]) / 2;
-                break;
-            case 7:     //7为底端居左
-                $posX = 0;
-                $posY = $groundInfo["height"] - $waterInfo["height"];
-                break;
-            case 8:     //8为底端居中
-                $posX = ($groundInfo["width"] - $waterInfo["width"]) / 2;
-                $posY = $groundInfo["height"] - $waterInfo["height"];
-                break;
-            case 9:     //9为底端居右
-                $posX = $groundInfo["width"] - $waterInfo["width"];
-                $posY = $groundInfo["height"] - $waterInfo["height"];
-                break;
-            case 0:
-            default:    //随机
-                $posX = rand(0,($groundInfo["width"] - $waterInfo["width"]));
-                $posY = rand(0,($groundInfo["height"] - $waterInfo["height"]));
-                break;
-        }
-        return array("posX"=>$posX, "posY"=>$posY);
-    }
-
-    /* 内部使用的私有方法，用于获取图片的属性信息（宽度、高度和类型） */
-    private function getInfo($name, $path=".") {
-        $spath = $path=="." ? rtrim($this->path,"/")."/" : $path.'/';
-
-        $data = getimagesize($spath.$name);
-        $imgInfo["width"]  = $data[0];
-        $imgInfo["height"] = $data[1];
-        $imgInfo["type"]  = $data[2];
-
-        return $imgInfo;
-    }
-
-    /*内部使用的私有方法， 用于创建支持各种图片格式（jpg,gif,png三种）资源 */
-    private function getImg($name, $imgInfo, $path='.'){
-
-        $spath = $path=="." ? rtrim($this->path,"/")."/" : $path.'/';
-        $srcPic = $spath.$name;
-
-        switch ($imgInfo["type"]) {
-            case 1:         //gif
-                $img = imagecreatefromgif($srcPic);
-                break;
-            case 2:         //jpg
-                $img = imagecreatefromjpeg($srcPic);
-                break;
-            case 3:         //png
-                $img = imagecreatefrompng($srcPic);
-                break;
-            default:
-                return false;
-                break;
-        }
-        return $img;
-    }
-
-    /* 内部使用的私有方法，返回等比例缩放的图片宽度和高度，如果原图比缩放后的还小保持不变 */
-    private function getNewSize($name, $width, $height, $imgInfo){
-        $size["width"] = $imgInfo["width"];     //原图片的宽度
-        $size["height"] = $imgInfo["height"];    //原图片的高度
-
-        if($width < $imgInfo["width"]){
-            $size["width"]=$width;          //缩放的宽度如果比原图小才重新设置宽度
-        }
-
-        if($height < $imgInfo["height"]){
-            $size["height"] = $height;        //缩放的高度如果比原图小才重新设置高度
-        }
-        /* 等比例缩放的算法 */
-        if($imgInfo["width"]*$size["width"] > $imgInfo["height"] * $size["height"]){
-            $size["height"] = round($imgInfo["height"]*$size["width"]/$imgInfo["width"]);
-        }else{
-            $size["width"] = round($imgInfo["width"]*$size["height"]/$imgInfo["height"]);
-        }
-
-        return $size;
-    }
-
-    /* 内部使用的私有方法，用于保存图像，并保留原有图片格式 */
-    private function createNewImage($newImg, $newName, $imgInfo){
-        $this->path = rtrim($this->path,"/")."/";
-        switch ($imgInfo["type"]) {
-            case 1:       //gif
-                $result = imageGIF($newImg, $this->path.$newName);
-                break;
-            case 2:       //jpg
-                $result = imageJPEG($newImg,$this->path.$newName);
-                break;
-            case 3:       //png
-                $result = imagePng($newImg, $this->path.$newName);
-                break;
-        }
-        imagedestroy($newImg);
-        return $newName;
-    }
-
-    /* 内部使用的私有方法，用于加水印时复制图像 */
-    private function copyImage($groundImg, $waterImg, $pos, $waterInfo){
-        imagecopy($groundImg, $waterImg, $pos["posX"], $pos["posY"], 0, 0, $waterInfo["width"],$waterInfo["height"]);
-        imagedestroy($waterImg);
-        return $groundImg;
-    }
-
-    /* 内部使用的私有方法，处理带有透明度的图片保持原样 */
-    private function kidOfImage($srcImg, $size, $imgInfo){
-        $newImg = imagecreatetruecolor($size["width"], $size["height"]);
-        $otsc = imagecolortransparent($srcImg);
-        if( $otsc >= 0 && $otsc < imagecolorstotal($srcImg)) {
-            $transparentcolor = imagecolorsforindex( $srcImg, $otsc );
-            $newtransparentcolor = imagecolorallocate(
-                $newImg,
-                $transparentcolor['red'],
-                $transparentcolor['green'],
-                $transparentcolor['blue']
-            );
-            imagefill( $newImg, 0, 0, $newtransparentcolor );
-            imagecolortransparent( $newImg, $newtransparentcolor );
-        }
-        imagecopyresized( $newImg, $srcImg, 0, 0, 0, 0, $size["width"], $size["height"], $imgInfo["width"], $imgInfo["height"] );
-        imagedestroy($srcImg);
-        return $newImg;
+    public function __destruct()
+    {
+        imagedestroy($this->_resource);
     }
 }
